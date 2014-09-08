cc78167db07d3a5d9cf076c218a5503d34265fc7
resilience-me <emailsareobsolete@gmail.com>
2014-09-08 03:31:58 +0200

    Update account_tx_history.js

--- Code/account_tx_history.js
+++ Code/account_tx_history.js
@@ -1,5 +1,11 @@
-//lists an accounts outgoing and incoming transactions
-//filter by currency
+//lists an accounts incoming transactions
+//for all currencies the account has connected
+//and declares tax for each of transaction
+
+
+
+//================ connect to Ripple ================
+
 
 
 var ripple = require('ripple-lib');
@@ -22,36 +28,96 @@ remote.connect(function() {
         'account': "rLaKjMvLbrAJwnH4VpawQ6ot9epZqJmbfQ",
         'ledger_index_min': -1,
         'ledger_index_max': -1,
-        'limit': 20
+        'limit': 50
     };
+    
+    
+//================ Create wallet ================
+//should be encoded with ripple-account secret key
+             
+    var wallet = [
+        {
+        'currency':'RES',
+        'taxRate': '0020000000'
+        },
+        
+        {
+        'currency':'USD',
+        'taxRate': '0004000000'
+        },
+      
+        {
+        'currency':'EUR',
+        'taxRate': '0010000000'
+        },
+   
+    ];
+    
+    
+   
+   
+//================ request account_tx ================
+
+    
 
     remote.request_account_tx(params)
         .on('success', function(data) {
 
-            console.log("++++++++++");
-            console.log("marker.ledger=" + data.marker.ledger);
-            console.log("marker.seq=" + data.marker.seq);
-            console.log("++++++++++");
 
             for (var i=0; i < data.transactions.length; i++) {
 
-                console.log("===========");
                 var tx = data.transactions[i].tx;
-                var meta = data.transactions[i].meta;
-                var timestamp = ripple.utils.toTimestamp(data.transactions[i].tx.date);
-                var date = new Date(timestamp);
 
-                if (tx.TransactionType === 'Payment' && tx.Amount.currency === "RES") {
-                    console.log("dateLocale=" + date.toLocaleString());
-                    console.log("TransactionResult=" + meta.TransactionResult);
-                    console.log("TransactionType=" + tx.TransactionType);
+
+
+//================ wallet.currency ================
+
+
+     var IOU1 = wallet[0].currency;
+     var IOU2 = wallet[1].currency;
+     var IOU3 = wallet[2].currency;
+     var IOU = (IOU1 + ", "  +  IOU2 + ", " + IOU3);
+
+
+             
+
+
+
+
+//================ Incoming Payments ================
+
+                if (tx.TransactionType === 'Payment' &&  IOU.indexOf(tx.Amount.currency) > -1 && tx.Destination === params.account) {
+                    console.log("Incoming Payment");
                     console.log("Account=" + tx.Account);
-                    console.log("Amount=" + ripple.Amount.from_json(tx.Amount).to_text_full());
+                    console.log("Amount=" + tx.Amount.value);
                     console.log("Currency=" + tx.Amount.currency);
-
+                    console.log("Issuer=" + tx.Amount.issuer);
                     console.log("Destination=" + tx.Destination);
-                    console.log("Ledger Index=" + tx.ledger_index);
+                    //================ wallet.taxRate ================
+
+                    if (tx.Amount.currency === IOU1) {
+                       console.log("taxRate=" + wallet[0].taxRate) 
+                    };
+
+                    if (tx.Amount.currency === IOU2) {
+                       console.log("taxRate=" + wallet[1].taxRate) 
+                    };
+            
+                    if (tx.Amount.currency === IOU3) {
+                       console.log("taxRate=" + wallet[2].taxRate) 
+                    };
+                        
+            
+                    console.log("");
+                    console.log("===========");
+                    console.log("");
+                    
+                    
+
+
+
                 }
+                
             }
 
         }).request();
