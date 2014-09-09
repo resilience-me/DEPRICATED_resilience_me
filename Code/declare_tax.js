5abe0c684af1036715fcaf571134349194603b1c
resilience-me <emailsareobsolete@gmail.com>
2014-09-09 18:37:27 +0200

    Update declare_tax.js

--- Code/declare_tax.js
+++ Code/declare_tax.js
@@ -3,32 +3,6 @@
 //and declares tax for the latest transaction
 
 
-//================ connect to Ripple ================
-
-
-
-var ripple = require('ripple-lib');
-var util   = require('util');
-
-var remote = new ripple.Remote({
-    trace: false,
-    trusted:  true,
-    local_signing:  true,
-    servers: [{
-        host:    's1.ripple.com'
-        , port:    443
-        , secure:  true
-    }]
-});
-
-remote.connect(function() {
-
-    var params = {
-        'account': "rLaKjMvLbrAJwnH4VpawQ6ot9epZqJmbfQ",
-        'ledger_index_min': -1,
-        'ledger_index_max': -1,
-        'limit': 50
-    };
 
 
 //================ Create wallet ================
@@ -52,8 +26,17 @@ remote.connect(function() {
         },
 
     ];
+    
+//================ create a string with wallet.currency ================
 
-//================ declare_tax BLOB ================
+     var IOUs = "";
+
+for (var ii = 0; ii < wallet.length; ii++) { 
+    IOUs = IOUs + wallet[ii].currency + " ";
+}
+
+
+//================ BLOB for declare_tax ================
 
 
      var TAX_BLOB = {
@@ -68,17 +51,51 @@ remote.connect(function() {
      };
 
 
-//================ declare poignant variables ================
+//================ some poignant variables ================
      var r=0;
      var taxRate;
      var tax_amount;
+     
+//================ account_tx params ================
+
+     
+      var params = {
+        'account': "rLaKjMvLbrAJwnH4VpawQ6ot9epZqJmbfQ",
+        'ledger_index_min': -1,
+        'ledger_index_max': -1,
+        'limit': 50
+    };
 
 
-//================ account_tx from remote ================
+
+//================ connect to Ripple ================
+
+
+var ripple = require('ripple-lib');
+var util   = require('util');
+
+var remote = new ripple.Remote({
+    trace: false,
+    trusted:  true,
+    local_signing:  true,
+    servers: [{
+        host:    's1.ripple.com'
+        , port:    443
+        , secure:  true
+    }]
+});
+
+
+remote.connect(function() {
+
+   
+
+
+
+//================ get an accounts transaction history from ripple ================
 
     remote.request_account_tx(params)
         .on('success', function(data) {
-
 
 
 
@@ -90,34 +107,22 @@ remote.connect(function() {
 
 
 
-//================ create a string with wallet.currency ================
-
-     var IOU = wallet[0].currency + wallet[1].currency + wallet[2].currency;
-
-
-
 
 
 
 //================ filter account_tx by incoming payments and wallet.currency ================
-
-                if (tx.TransactionType === 'Payment' &&  IOU.indexOf(tx.Amount.currency) > -1 && tx.Destination === params.account) {
-
+                if (tx.TransactionType === 'Payment' &&  IOUs.indexOf(tx.Amount.currency) > -1 && tx.Destination === params.account) {
 
 
 //================ set taxRate ================
+for (var iii = 0; iii < wallet.length; iii++) { 
+    
+    if (tx.Amount.currency === wallet[iii].currency) {
+    taxRate = wallet[iii].taxRate;
+    }
+    
+}
 
-                    if (tx.Amount.currency === wallet[0].currency) {
-                        taxRate = wallet[0].taxRate;
-                    }
-
-                    if (tx.Amount.currency === wallet[1].currency) {
-                        taxRate = wallet[1].taxRate;
-                    }
-
-                    if (tx.Amount.currency === [2].currency) {
-                        taxRate = wallet[2].taxRate;
-                    }
 
 
 
@@ -138,7 +143,7 @@ remote.connect(function() {
 
                     //this sets how many transactions to declare_tax for
                     //this is set with the variable r
-                    if (r <= 2) {
+                    if (r <= 10) {
                         //these declare_tax objects are sent to resilience.me
                         //output should be sent to resilience.me-server
                         var output = JSON.stringify(TAX_BLOB, null, 2);
